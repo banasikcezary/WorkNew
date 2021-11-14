bcf8c0106798412f9add86e0da7caad6b8554467
banasikcezary@gmail.com <Superzenek1!>
2021-11-14 21:50:09 +0100

    new conf 14.11 3

--- src/test/java/utils/ScreenShotMaker.java
+++ src/test/java/utils/ScreenShotMaker.java
@@ -1,14 +1,11 @@
 package utils;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import driver.manager.DriverManager;
-import gherkin.ast.Scenario;
-import io.qameta.allure.Allure;
+
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.io.ByteArrayInputStream;
 
 public class ScreenShotMaker {
 
