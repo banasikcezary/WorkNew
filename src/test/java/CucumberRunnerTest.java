b47bbd58d9686977709b38a8899a3f8e1e97b793
banasikcezary@gmail.com <Superzenek1!>
2021-11-14 14:27:44 +0100

    new conf 14.11 3

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,12 +1,7 @@
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.qameta.allure.Attachment;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
-import utils.ScreenShotMaker;
-import utils.testng.listeners.RetryAnalyzer;
-import utils.testng.listeners.TestListener;
+
 
 
 @CucumberOptions(features = {"src/test/resources/"},
