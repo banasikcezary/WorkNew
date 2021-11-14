a163dea7cded6c19f71e72370b393ecb3375f449
banasikcezary@gmail.com <Superzenek1!>
2021-11-14 14:04:36 +0100

    new conf 14.11 3

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,11 +1,13 @@
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.qameta.allure.Attachment;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import utils.testng.listeners.RetryAnalyzer;
 import utils.testng.listeners.TestListener;
 
+@Listeners(TestListener.class)
 @CucumberOptions(features = {"src/test/resources/"},
         glue={"Test"},
         plugin = { "pretty",
