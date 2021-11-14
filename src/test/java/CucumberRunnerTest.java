46e95cb092d251ed0fd3ce3bd077104f2e8646d7
banasikcezary@gmail.com <Superzenek1!>
2021-11-14 01:14:17 +0100

    new conf 13.11 7

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -4,11 +4,11 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Listeners;
 import utils.testng.listeners.TestListener;
 
-@Listeners(TestListener.class)
 @CucumberOptions(features = {"src/test/resources/"},
         glue={"Test"},
-        plugin = { "pretty",
-        "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"}
+        plugin = { "pretty"
+
+}
 
       )
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
