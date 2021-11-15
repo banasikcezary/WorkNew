574a052846d6550ba87f8b563c0e4cbbadc2c060
banasikcezary@gmail.com <Superzenek1!>
2021-11-15 20:46:11 +0100

    new conf 14.11 3

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,10 +1,11 @@
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Listeners;
+import utils.testng.listeners.TestListener;
 
 
-
-
+@Listeners(TestListener.class)
 @CucumberOptions(features = {"src/test/resources/"},
         glue={"Test"},
         plugin = { "pretty",
