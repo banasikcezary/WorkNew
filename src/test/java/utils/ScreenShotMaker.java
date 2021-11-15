c6dc2764bb17cc61fa3aa1a290c3a56b6a3e8baf
banasikcezary@gmail.com <Superzenek1!>
2021-11-15 17:05:54 +0100

    new conf 14.11 3

--- src/test/java/utils/ScreenShotMaker.java
+++ src/test/java/utils/ScreenShotMaker.java
@@ -1,34 +1,23 @@
 package utils;
 
-import com.codeborne.selenide.Screenshots;
 import driver.manager.DriverManager;
 
-import io.cucumber.core.api.Scenario;
-import io.cucumber.java.AfterStep;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 
 public class ScreenShotMaker {
-    @AfterStep
+
     public static void makeScreenShot() {
-       // Allure.addAttachment("Fail test",new ByteArrayInputStream(((TakesScreenshot)DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES)));
-
-                try {
-                    Allure.addAttachment("Failed step", new FileInputStream(Screenshots.takeScreenShotAsFile()));
-                } catch (Exception e) {
-                    System.out.println(e);
-                }
-            }
-
-
+        Allure.addAttachment("Fail test",new ByteArrayInputStream(((TakesScreenshot)DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES)));
+//        byte[] screenShot = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+//        Allure.getLifecycle().addAttachment(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yy_hh:mm:ss")), "image/png", "png", screenShot);
+    }
 
 }
