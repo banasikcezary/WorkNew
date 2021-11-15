c3358384d99a5cd569c5bdd4328609680c73da03
banasikcezary@gmail.com <Superzenek1!>
2021-11-15 21:57:04 +0100

    new conf 14.11 3

--- src/test/java/utils/ScreenShotMaker.java
+++ src/test/java/utils/ScreenShotMaker.java
@@ -2,17 +2,35 @@ package utils;
 
 import driver.manager.DriverManager;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class ScreenShotMaker {
 
     @Attachment(value = "Page screenshot test failure", type = "image/png")
-    public static byte[] makeScreenShot() {
+    public static byte[] makeScreenShot()  {
         byte[] screenshotFile = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+
+try {
+    File screenshotAs = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.FILE);
+    Allure.addAttachment("Screenshot", FileUtils.openInputStream(screenshotAs));
+}catch (Exception e){
+    System.out.println(e);
+}
         return screenshotFile;
     }
+
 
 
 }
