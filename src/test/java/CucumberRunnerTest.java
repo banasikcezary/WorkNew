87f7e8faf7b973fb2e79d699555990b6db7bfd76
banasikcezary@gmail.com <Superzenek1!>
2021-11-15 20:19:55 +0100

    new conf 14.11 3

--- src/test/java/CucumberRunnerTest.java
+++ src/test/java/CucumberRunnerTest.java
@@ -1,18 +1,17 @@
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.Listeners;
-import utils.testng.listeners.TestListener;
+
 
 
 
 @CucumberOptions(features = {"src/test/resources/"},
         glue={"Test"},
         plugin = { "pretty",
-        "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"
+        "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"
 }
 
-      )
+)
 public class CucumberRunnerTest extends AbstractTestNGCucumberTests {
 
 
--- src/test/java/utils/testng/listeners/TestListener.java
+++ src/test/java/utils/testng/listeners/TestListener.java
@@ -1,11 +1,10 @@
 package utils.testng.listeners;
 
-import driver.manager.DriverManager;
-import io.qameta.allure.Allure;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
