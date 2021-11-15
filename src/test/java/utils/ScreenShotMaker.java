61c2226a1f4641a8233fadd70cc97bf7d0475dc7
banasikcezary@gmail.com <Superzenek1!>
2021-11-15 20:43:29 +0100

    new conf 14.11 3

--- src/test/java/utils/ScreenShotMaker.java
+++ src/test/java/utils/ScreenShotMaker.java
@@ -2,18 +2,22 @@ package utils;
 
 import driver.manager.DriverManager;
 
+import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.io.ByteArrayInputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class ScreenShotMaker {
 
-    @Attachment(value = "Page screenshot test failure", type = "image/png")
-    public static byte[] makeScreenShot() {
-        byte[] screenshotFile = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
-        return screenshotFile;
+    public static void makeScreenShot() {
+        Allure.addAttachment("Fail test",new ByteArrayInputStream(((TakesScreenshot)DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES)));
+//        byte[] screenShot = ((TakesScreenshot) DriverManager.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+//        Allure.getLifecycle().addAttachment(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yy_hh:mm:ss")), "image/png", "png", screenShot);
     }
 
 }
